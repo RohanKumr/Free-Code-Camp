d0ed0d42a0f079da43251d9145f8144730feee2f
Rohan Kumar <rkrohanrk065@gmail.com>
2022-05-04 21:25:03 +0530

    Update profile-lookup.js

--- javascript-algorithms-and-data-structures/basic-javscript/profile-lookup.js
+++ javascript-algorithms-and-data-structures/basic-javscript/profile-lookup.js
@@ -1,1 +1,50 @@
 
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  let nameExists = null;
+  let propExists = null;
+  let propValue = null;
+  for (let i = 0; i <= contacts.length - 1; i++) {
+    const c = contacts[i];
+    if (c.firstName === name) {
+      nameExists = name;
+      if (c.hasOwnProperty(prop)) {
+        propValue = c[prop];
+      }
+    }
+  }
+  return (
+    propValue ||
+    (!nameExists && "No such contact") ||
+    (!propExists && "No such property")
+  );
+}
+
+console.log(lookUpProfile("Kristian", "lastName"));
